a33e51e9172a83ef683d611fecaed60441243ade
kwangminini <rhkdals7362@gmail.com>
2022-10-30 23:38:37 +0900

    add Create and Read API

--- CatDataExample/src/app.ts
+++ CatDataExample/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { Cat, CatType } from "app.model";
+import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 const port: number = 8000;
 
@@ -10,6 +10,9 @@ app.use(
     next();
   }
 );
+
+//body값의 JSON을 읽기 위한 미들웨어 추가
+app.use(express.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
@@ -34,6 +37,64 @@ app.get("/", (req: express.Request, res: express.Response) => {
 // );
 app.get("/cats/som", (req: express.Request, res: express.Response) => {
   res.send({ som: Cat[1] });
+});
+
+//고양이 데이터 전부 조회
+app.get("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//특정 고양이 데이터 조회
+app.get("/cats/:id", (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//고양이 데이터 추가
+app.post("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    Cat.push(data); //Create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 //위에 라우터에서 안걸렸을 때 (에러 라우터에 걸리는 미들웨어)

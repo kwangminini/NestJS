6ff36d523258d1e7bc38cd8d5d9228c4fc0b710e
kwangminini <rhkdals7362@gmail.com>
2022-11-06 02:24:47 +0900

    update singleton pattern

--- CatDataExample/src/app.ts
+++ CatDataExample/src/app.ts
@@ -4,26 +4,56 @@ import catsRouter from "./cats/cats.route";
 const app: express.Express = express();
 const port: number = 8000;
 
-//전체 라우터에 걸리는 미들웨어
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("req :::", req.rawHeaders[1]);
-    next();
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
   }
-);
 
-//body값의 JSON을 읽기 위한 미들웨어 추가
-app.use(express.json());
-
-app.use(catsRouter);
-
-//위에 라우터에서 안걸렸을 때 (에러 라우터에 걸리는 미들웨어)
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send({ error: `404 not found` });
+  private setRoute() {
+    this.app.use(catsRouter);
   }
-);
+  private setMiddleware() {
+    //전체 라우터에 걸리는 미들웨어
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        console.log("req :::", req.rawHeaders[1]);
+        next();
+      }
+    );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    //body값의 JSON을 읽기 위한 미들웨어 추가
+    this.app.use(express.json());
+    this.setRoute();
+    //위에 라우터에서 안걸렸을 때 (에러 라우터에 걸리는 미들웨어)
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        res.send({ error: `404 not found` });
+      }
+    );
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();

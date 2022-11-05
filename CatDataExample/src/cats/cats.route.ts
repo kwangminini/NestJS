886044963a1a72eb981f3ea23af86823a6c9eed9
kwangminini <rhkdals7362@gmail.com>
2022-11-06 02:59:05 +0900

    update design pattern

--- CatDataExample/src/cats/cats.route.ts
+++ CatDataExample/src/cats/cats.route.ts
@@ -1,5 +1,13 @@
 import { Cat, CatType } from "./cats.model";
 import { Router, Request, Response } from "express";
+import {
+  createCat,
+  deleteCat,
+  readAllcat,
+  readCat,
+  updateCat,
+  updatePartialCat,
+} from "./cats.service";
 
 const router = Router();
 
@@ -13,131 +21,21 @@ router.get("/cats/som", (req: Request, res: Response) => {
 });
 
 //고양이 데이터 전부 조회
-router.get("/cats", (req: Request, res: Response) => {
-  try {
-    const cats = Cat;
-    // throw new Error("db connect error");
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.get("/cats", readAllcat);
 
 //특정 고양이 데이터 조회
-router.get("/cats/:id", (req: Request, res: Response) => {
-  try {
-    const params = req.params;
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    // throw new Error("db connect error");
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.get("/cats/:id", readCat);
 
 //고양이 데이터 추가
-router.post("/cats", (req: Request, res: Response) => {
-  try {
-    const data = req.body;
-    Cat.push(data); //Create
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.post("/cats", createCat);
 
 //고양이 데이터 수정
-router.put("/cats:id", (req: Request, res: Response) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-    let result;
-    Cat.map((cat) => {
-      if (cat.id === params.id) {
-        cat = body;
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.put("/cats:id", updateCat);
 
 //고양이 데이터 부분적으로 업데이트
-router.patch("/cats:id", (req: Request, res: Response) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-    let result;
-    Cat.map((cat) => {
-      if (cat.id === params.id) {
-        cat = { ...cat, ...body };
-        result = cat;
-      }
-    });
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.patch("/cats:id", updatePartialCat);
 
 //고양이 데이터 삭제
-router.delete("/cats:id", (req: Request, res: Response) => {
-  try {
-    const params = req.params;
-    const newCat = Cat.filter((cat) => cat.id !== params.id);
-
-    res.status(200).send({
-      success: true,
-      data: newCat,
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+router.delete("/cats:id", deleteCat);
 
 export default router;
---
+++ CatDataExample/src/cats/cats.service.ts
@@ -0,0 +1,143 @@
+import { Cat, CatType } from "./cats.model";
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  console.log(req);
+  res.send({ cats: Cat });
+});
+
+// router.get("/cats/som", (req: Request, res: Response) => {
+//   res.send({ som: Cat[1] });
+// });
+
+//고양이 데이터 전부 조회
+export const readAllcat = (req: Request, res: Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//특정 고양이 데이터 조회
+export const readCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    // throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//고양이 데이터 추가
+export const createCat = (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    Cat.push(data); //Create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//고양이 데이터 수정
+export const updateCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.map((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//고양이 데이터 부분적으로 업데이트
+export const updatePartialCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.map((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+//고양이 데이터 삭제
+export const deleteCat = (req: Request, res: Response) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export default router;
